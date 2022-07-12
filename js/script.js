13fe875def99c10596b73482fb5116c5d852c845
Gonzalez75 <guilherme75gonzalez@gmail.com>
2022-07-12 14:57:11 -0300

    commit #7

--- js/script.js
+++ js/script.js
@@ -13,19 +13,29 @@ form.addEventListener('submit', function(e){
 
     let answer = document.getElementById("content")
 
+    let image = document.getElementById('imgPokemon')
+
     let html = ''
 
     fetch(urlForm)
         .then(answer => answer.json())
         .then(function (data){
             console.log(data)
-            html = 'Nome:' + data.name + '<br>'
-            html = html + 'Type: ' + data.types[0].type.name
+            html = 'Nome:' + uppercase(data.name) + '<br>'
+            html = html + 'Type: ' + uppercase(data.types[0].type.name)
             answer.innerHTML = html
+            image.innerHTML = "<img src='" + data.sprites.front_default + "'><img src='" + data.sprites.back_default + "'>"
         })
         .catch(function (err){
-            console.log(err)
+            if(err == 'SyntaxError: Unexpected token N in JSON at position 0'){
+                html = 'Pokémon not found!'
+            } else {
+                html = 'Error:' + err
+            }
+            answer.innerHTML = html
         })
 })
 
-function
+function uppercase(val){
+    return val[0].toUpperCase() + val.substr(1)
+}
